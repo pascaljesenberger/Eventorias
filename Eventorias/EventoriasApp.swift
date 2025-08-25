6c85d8303a89c8be87659e39d3a4137fe3dd633d
pascaljesenberger <pascal@jesenberger.fr>
2025-08-26 01:39:12 +0200

    Initial Commit

---
+++ Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Pascal Jesenberger on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EventoriasTests/EventoriasTests.swift
@@ -0,0 +1,16 @@
+//
+//  EventoriasTests.swift
+//  EventoriasTests
+//
+//  Created by Pascal Jesenberger on 26/08/2025.
+//
+
+import Testing
+
+struct EventoriasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
