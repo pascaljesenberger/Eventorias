7e0e12d6d39d5964ae120ac5d06962e77aa4eedb
pascaljesenberger <pascal@jesenberger.fr>
2025-08-31 05:16:20 +0200

    add ErrorText

--- Eventorias/Views/Components/ErrorMessage.swift
+++ Eventorias/Views/Components/ErrorMessage.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ErrorMessage: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             Circle()
                 .foregroundColor(.appDarkGray)
                 .frame(width: 64, height: 64)
@@ -19,13 +19,30 @@ struct ErrorMessage: View {
                         .scaledToFit()
                         .frame(width: 5, height: 24)
                 )
+                .padding(.bottom)
             
             Text("Error")
+                .font(.system(size: 20, weight: .semibold))
+                .foregroundColor(.white)
             
+            Text("An error has occured, please try again later")
+                .font(.system(size: 16, weight: .regular))
+                .foregroundColor(.white)
+                .frame(width: 164)
+                .multilineTextAlignment(.center)
+            
+            CustomButton(
+                action: { print("") },
+                title: "Try again",
+                width: 159,
+                height: 40
+            )
+            .padding(.top, 24)
         }
     }
 }
 
 #Preview {
     ErrorMessage()
+        .appBackground
 }
